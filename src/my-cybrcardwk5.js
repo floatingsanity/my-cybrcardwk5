28e7ab8856beaf75a9555d0e44f1a202db0017f9
Feda Almodhi <143366228+floatingsanity@users.noreply.github.com>
2023-12-07 00:03:44 -0500

    update

--- src/my-cybrcardwk5.js
+++ src/my-cybrcardwk5.js
@@ -1,52 +1,56 @@
 import { LitElement, html, css } from 'lit';
 
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
-
 class MyCybrcardwk5 extends LitElement {
   static properties = {
     header: { type: String },
-  }
+  };
 
   static styles = css`
     :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
+      font-family: Arial, sans-serif;
       text-align: center;
-      background-color: var(--my-cybrcardwk5-background-color);
+      background-color: #f4f4f4;
+      margin: 0;
+      padding: 0;
+      display: block;
     }
 
-    main {
-      flex-grow: 1;
+    .container {
+      max-width: 400px;
+      margin: 0 auto;
+      padding: 20px;
+      background-color: #fff;
+      border-radius: 10px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
     }
 
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
+    h1 {
+      font-size: 36px;
+      margin-bottom: 20px;
     }
 
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
+    img {
+      max-width: 100%;
+      height: auto;
     }
 
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
+    .btn {
+      background-color: brown; /* Brown button color */
+      color: #fff;
+      border: none;
+      padding: 10px 20px;
+      font-size: 18px;
+      cursor: pointer;
+      border-radius: 5px;
     }
 
-    .app-footer a {
-      margin-left: 5px;
+    .btn:hover,
+    .btn:focus {
+      background-color: #8b4513; /* Darker brown on hover/focus */
+    }
+
+    .hidden {
+      display: none;
     }
   `;
 
@@ -55,32 +59,56 @@ class MyCybrcardwk5 extends LitElement {
     this.header = 'My app';
   }
 
+  toggleDetails() {
+    const description = this.shadowRoot.querySelector('.hidden');
+    const imdbLink = this.shadowRoot.querySelector('a.hidden');
+    description.classList.toggle('hidden');
+    imdbLink.classList.toggle('hidden');
+  }
+
+  changeTitle() {
+    const title = this.shadowRoot.querySelector('h1');
+    title.textContent = 'Something Else';
+  }
+
+  resetTitle() {
+    const title = this.shadowRoot.querySelector('h1');
+    title.textContent = 'Memento';
+  }
+
+  duplicateCard() {
+    const container = this.shadowRoot.querySelector('.container');
+    const clonedCard = container.cloneNode(true);
+    this.shadowRoot.querySelector('main').insertAdjacentElement('beforeend', clonedCard);
+  }
+
+  removeCard() {
+    const cards = this.shadowRoot.querySelectorAll('.container');
+    if (cards.length > 1) {
+      const lastCard = cards[cards.length - 1];
+      lastCard.remove();
+    }
+  }
+
   render() {
     return html`
       <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
+        <div class="container">
+          <h1>Memento</h1>
+          <img src="https://m.media-amazon.com/images/I/515EOVqRexL._AC_UF894,1000_QL80_.jpg" alt="Movie Poster">
+          <p class="hidden">This is a complex story about Leonard Shelby (Guy Pearce), a man whose ability to make new memories is damaged when he is struck in the head while confronting two people who are attacking his wife (Jorja Fox) at their home in the middle of the night.</p>
+          <a href="https://www.imdb.com/title/tt0209144/" target="_blank" class="hidden">
+            <button class="btn">Visit IMDB</button>
+          </a>
+        </div>
 
-        <p>Edit <code>src/MyCybrcardwk5.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
+        <button @click="${this.toggleDetails}" class="btn">Toggle Details</button>
+        <button @click="${this.duplicateCard}" class="btn">Duplicate Card</button>
+        <button @click="${this.removeCard}" class="btn">Remove Card</button>
+        <button @click="${this.changeTitle}" class="btn">Change Title</button>
+        <button @click="${this.resetTitle}" class="btn">Reset Title</button>
       </main>
 
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
     `;
   }
 }
